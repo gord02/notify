07e364a4b1a0ad1be9440c5a47dd08d8d2d9b8a7
gord02 <gordon.hamilton1110@gmail.com>
2023-03-05 22:06:22 -0500

    added snowflake and drw

---
+++ companies/snowflake.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+from selenium import webdriver
+# from seleniumwire import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+import sys
+import time
+sys.path.insert(0,'..') #this works relative to where to program was run from 
+
+from logic import process
+from logic import notify
+from logic import sqlQueries
+
+def get_data(): 
+    company =  "DRW"
+    opts = Options()
+    # so that browser instance doesn't pop up
+    opts.add_argument("--headless")
+    jobs = []
+
+    try:
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options = opts)
+        url = "https://careers.snowflake.com/us/en/search-results?rk=l-university-recruiting&sortBy=Most%20relevant"
+        driver.get(url)
+
+        content = driver.page_source
+        soup = BeautifulSoup(content, "lxml")
+        driver.quit()
+        elements = soup.select("div.job-title > span")
+        locations = soup.select("span.job-location")
+        
+        for i, element in enumerate (elements):            
+            jobs.append(element.contents[0] + " (" + locations[i].contents[8].strip()+ ")")
+                     
+        jobs = process.process_job_titles(jobs)
+        if len(jobs) > 0:
+            # update company in database to found
+            sqlQueries.update_company(company)
+        return jobs
+        
+    except Exception as e:
+        # send email about scrapping error
+        error=f"Exception parsing {company} "+ e
+        print(error)
+        notify.parsing_error(error)
+        return jobs
+        
+# get_data()
